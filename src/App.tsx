52ff157c1e9d1ce54438ada39381aa3238182cb5
puppyspot54321-cmyk <puppyspot54321@gmail.com>
2026-09-08 23:56:29 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import type { Session } from '@supabase/supabase-js'
-import AuthScreen from './Auth'
 import heroImage from './assets/hero.png'
 import { useEffect, useMemo, useRef, useState } from 'react'
 import type { Movie } from './movieData/movies'
 import { supabase } from './supabase'
+import type { Session } from '@supabase/supabase-js'
+import AuthScreen from './Auth'
 
 type Section = 'home' | 'movies' | 'series' | 'my-list'
 
