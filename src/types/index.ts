09e89f2b5521598d63437be4dbbbf670a3576116
puppyspot54321-cmyk <puppyspot54321@gmail.com>
2026-09-09 15:11:42 +0100

    Create index.ts to export type definitions

---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+export type { Genre, Subgenre } from './genre'
+export type { Region } from './region'
+export type { Country } from './country'
+export type { FilmIndustry } from './industry'
+export type { Language } from './language'
+export type { Tag } from './tag'
+export type { Collection } from './collection'
+export type { ContentType, AgeRating } from './content'
+export type {
+  VideoQuality,
+  StreamingProtocol,
+  VideoAsset,
+  SubtitleTrack,
+  AudioTrack,
+} from './media'
+export type { Person, CastMember } from './person'
+export type { Movie } from './movie'
