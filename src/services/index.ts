d4495dab8324923fbff13674400097cdbd714032
puppyspot54321-cmyk <puppyspot54321@gmail.com>
2026-09-11 00:32:50 +0100

    Add service layer exports

---
+++ src/services/index.ts
@@ -0,0 +1,21 @@
+export {
+  getGenres,
+  getSubgenres,
+  getRegions,
+  getCountries,
+  getFilmIndustries,
+  getLanguages,
+  getTags,
+  getCollections,
+  getAgeRatings,
+  getMetadataCatalog,
+} from './metadataService'
+
+export type {
+  MetadataCatalog,
+} from './metadataService'
+
+export {
+  getMovies,
+  getMovieById,
+} from './movieService'
